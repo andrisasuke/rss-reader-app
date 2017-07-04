48cf137124ab2f00467911355276b10e363e4545
andrisasuke <andri.sasuki@gmail.com>
2017-07-04 19:05:30 +0700

    change base snackbar varible to lateint

--- app/src/main/java/com/andrisasuke/app/cardnews/BaseActivity.kt
+++ app/src/main/java/com/andrisasuke/app/cardnews/BaseActivity.kt
@@ -12,7 +12,7 @@ import android.view.View
 
 abstract class BaseActivity : AppCompatActivity(), ToolbarManager {
 
-    protected var snackbar: Snackbar? = null
+    protected lateinit var snackbar: Snackbar
     protected val handler by lazy {  Handler() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,14 +20,11 @@ abstract class BaseActivity : AppCompatActivity(), ToolbarManager {
     }
 
     fun showSnackbar(view: View, message: String) {
-        if (this.snackbar != null && (this.snackbar?.isShown as Boolean) ){
-            this.snackbar?.dismiss()
-        }
         this.snackbar = Snackbar.make(view, message, Snackbar.LENGTH_SHORT)
                 .setAction(R.string.ok_lbl) {
-                   snackbar?.dismiss()
+                   snackbar.dismiss()
                 }
-        this.snackbar?.show()
+        this.snackbar.show()
     }
 
     fun postDelayed(r: Runnable, delayMillis: Long): Boolean {
